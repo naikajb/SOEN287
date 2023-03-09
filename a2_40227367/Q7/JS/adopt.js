4a1616085699bd0f1011f9ef8c80a9f970749f4c
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-03-09 14:09:46 -0500

    started browse page

--- a2_40227367/Q7/JS/adopt.js
+++ a2_40227367/Q7/JS/adopt.js
@@ -1,5 +1,5 @@
-setInterval(myTime, 1000);
-function myTime() {
+setInterval(showDate, 1000);
+function showDate() {
     const date = new Date();
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -13,15 +13,12 @@ function myTime() {
 
     let textDate = dayName + ", " + month +" " +day + ", " + year + "\t"+hour +":"+min+":"+sec;
     document.getElementById('date').innerHTML = textDate;
-    /*document.getElementById('date').innerHTML=day+'<sup>th</sup>';
-    document.getElementById('dayName').innerHTML=dayName;
-    document.getElementById('month').innerHTML=month;
-    document.getElementById('year').innerHTML=year;
-    document.getElementById('hour').innerHTML=hour+'h';
-    document.getElementById('mins').innerHTML=mins+'m';
-    document.getElementById('sec').innerHTML=sec+'s';*/
+
 }
 
+function showNextPet(){
+
+}
 function validate(){
     if(document.getElementById("breed").value == ""){
         alert('fill it out');
