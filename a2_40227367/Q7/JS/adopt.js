8f2bac40f3c78382192c93e9241374fa813619ec
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-03-09 13:08:23 -0500

    added date to header

--- a2_40227367/Q7/JS/adopt.js
+++ a2_40227367/Q7/JS/adopt.js
@@ -1,24 +1,26 @@
+setInterval(myTime, 1000);
+function myTime() {
+    const date = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let dayName = days[date.getDay()];
+    let day = date.getDate();
+    let month = months[date.getMonth()];
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
 
-function getDate(){
-    const months = ["January", "February","March", "April", "May", "June", "July", "August", "September", "October","November", "December"];
-
-    const days = ["Sunday",'Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-
-    const d = new Date();
-
-    let day = days[d.getDay()];
-    let month = months[d.getMonth];
-    let time = d.getHours() + ":" + d.getMinutes() +":"+ d.getSecond();
-
-    let dateAndTime = time +" "+day+", "+month+" " + d.getDate()+"," + d.getFullYear();
-
-    //return dateAndTime;
-    document.getElementById("date").innerHTML = dateAndTime;
-    setTimeout(getDate,1000);
+    let textDate = dayName + ", " + month +" " +day + ", " + year + "\t"+hour +":"+min+":"+sec;
+    document.getElementById('date').innerHTML = textDate;
+    /*document.getElementById('date').innerHTML=day+'<sup>th</sup>';
+    document.getElementById('dayName').innerHTML=dayName;
+    document.getElementById('month').innerHTML=month;
+    document.getElementById('year').innerHTML=year;
+    document.getElementById('hour').innerHTML=hour+'h';
+    document.getElementById('mins').innerHTML=mins+'m';
+    document.getElementById('sec').innerHTML=sec+'s';*/
 }
-getDate();
-
 
 function validate(){
     if(document.getElementById("breed").value == ""){
