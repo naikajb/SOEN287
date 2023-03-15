58645b61d0e9221a5be959a486d0c67812fbe19d
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-03-14 20:34:52 -0400

    added validation of find-pets page

--- a2_40227367/Q7/JS/adopt.js
+++ a2_40227367/Q7/JS/adopt.js
@@ -12,6 +12,14 @@ function showDate() {
     let min = date.getMinutes();
     let sec = date.getSeconds();
 
+    if(sec< 10) {
+        sec = "0" + sec;
+    }
+
+    if ( min < 10){
+        min = "0"+min;
+    }
+
     let textDate = dayName + ", " + month +" " +day + ", " + year + "\t"+hour +":"+min+":"+sec;
     document.getElementById('date').innerHTML = textDate;
 
@@ -34,14 +42,36 @@ function checkEmptyCat(){
     let z = document.forms["catForm"]["sex"].value;
     let k = document.forms["catForm"]["gets-along"].value;
 
-    if( x.length == 0 || y.length == 0|| z.length == 0|| k.length == 0){
+    if( x.length === 0 || y.length === 0|| z.length === 0|| k.length === 0){
         alert("Make sure all input fields have been filled out.")
     }
 
 }
+
+// to validate the pet-giveaway form
 function validate(){
-    if(document.getElementById("breed").value == ""){
-        alert('fill it out');
+    let type = document.forms["animalInfo"]["type"].value;
+    let breed = document.forms["animalInfo"]["breed"].value;
+    let comments = document.forms["animalInfo"]["comments"].value;
+    let age = document.forms["animalInfo"]["age"].value;
+    let sex = document.forms["animalInfo"]["sex"].value;
+    let anim = document.forms["animalInfo"]["suitableAnim"].value;
+    let kids = document.forms["animalInfo"]["suitableKids"].value;
+    let fname = document.forms["animalInfo"]["fname"].value;
+    let lname = document.forms["animalInfo"]["lname"].value;
+    let email = document.forms["animalInfo"]["email"].value;
+
+    if(type.length === 0 || breed.length === 0 || comments.length === 0 || age.length === 0|| sex.length === 0 ||anim.length === 0 || kids.length === 0 || kids.length === 0 || fname.length === 0 || lname.length === 0 || email.length === 0){
+        alert("Make sure all input fields have been filled out.");
     }
+
+
+    if(fname.search(/\d/)===-1 || lname.search(/\d/)=== -1) {
+        alert("No digits allowed in name fields. ");
+    }
+
+
+
+
 }
 
