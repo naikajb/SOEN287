5bd0d5c50c7a44cc55e9f69fafcd201401ea962f
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-04-14 14:22:58 -0400

    changed to use template header, sidebar and footer

---
+++ a3_40227367/Q4/Cat-Care.php
@@ -0,0 +1,84 @@
+    <!DOCTYPE html>
+    <html lang ="en">
+
+    <head>
+        <Title> A Cat and Dog for All</Title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
+        <Title> A Cat and Dog for All</Title>
+        <link rel="stylesheet" href="CSS/style.css">
+        <script type = "text/javascript" src="JS/adopt.js"></script>
+    </head>
+
+    <body>
+    <header>
+        <?php include 'header.php';?>
+    </header>
+
+    <div class="sidebar">
+        <?php include 'sidebar.php';?>
+    </div>
+
+    <div class="main">
+        <div class="content">
+            <h2>How to Take Care of Your Cat</h2>
+            <h5>
+                Have you recently added a feline friend to your family? Congratulations! We know you’ll be thrilled to
+                have your new cat in your home.
+                Read on for useful tips for new cat parents, and for those looking to brush up on their pet care skills.
+            </h5></br>
+            <div class = "dog-care">
+                <p>
+                    <img id="cat-image" alt="Women with cat" src="Images/cat-care_general-cat-care_body1-right.jpg"
+                         alt="cat"></br>
+                </p>
+            </div>
+            <div class = "care">
+                <h3>Feeding</h3>
+                <ul>
+                    <li>Cats need taurine, an amino acid, for their heart and eye health. </li>
+                    <li>Fresh and clean water is necessary at all times, and washing your cat;s water bowl on a daily is
+                        recommended.</li>
+                    <li>Treats should make up at most 10% of your cat's diet.</li>
+                    <li>Many people feed baby food to a cat or kitten who is refusing food or not feeling well.
+                        Please read labels carefully: If the baby food contains onion or garlic powder, your pet could
+                        be poisoned.</li>
+                    <li>Take your pet to your veterinarian if signs of anorexia, diarrhea, vomiting or lethargy continue
+                        for more than two days.</li>
+                </ul>
+            </div>
+            <div class ="care">
+                <h3>Scratching</h3>
+                <p>Cats need to scratch! When a cat scratches, the old outer nail sheath is pulled off and the sharp,
+                    smooth claws underneath are exposed.
+                    Cutting your cat’s nails every two to three weeks will keep them relatively blunt and less likely to
+                    harm the arms of both humans and furniture.
+                    Provide your cat with a sturdy scratching post, at least three feet high.
+                    The post should also be stable enough that it won't wobble during use, and should be covered with
+                    rough material such as sisal, burlap or tree bark. Many cats also like scratching pads.</p>
+            </div>
+            <div class ="care">
+                <h3>Cat Supply Checklist</h3>
+                <ul>
+                    <li>Cat food</li>
+                    <li>Food dish</li>
+                    <li>Water bowl</li>
+                    <li>Brush & comb</li>
+                    <li>Litter box and litter</li>
+                    <li>Scratching post or scratching pad</li>
+                    <li>Cat carrier and cat bed</li>
+                    <li>Safety cat collar with ID tag</li>
+                </ul>
+            </div>
+            <div>
+                <p class = "source">Source: “General Cat Care.” ASPCA, 2015, <a href="www.aspca.org/pet-care/cat-care/general-cat-care">www.aspca.org/pet-care/cat-care/general-cat-care</a></p>
+            </div>
+        </div></br>
+    </div>
+
+    <footer>
+      <?php include 'footer.php'; ?>
+    </footer>
+
+    </body>
+
+    </html>
---
+++ a3_40227367/Q4/Dog-Care.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
+            <link rel = "stylesheet" href = "CSS/style.css">
+            <script type = "text/javascript" src="JS/adopt.js"></script>
+            <Title> A Cat and Dog for All</Title>
+        </head>
+
+        <body>
+            <header>
+                <?php include 'header.php';?>
+            </header>
+
+            <div class = "sidebar">
+                <?php include 'sidebar.php'; ?>
+            </div>
+
+            <div class="main">
+                <div class="content">
+                    <h2>How to Take Care of Your Dog</h2>
+                    <h5>
+                        A dog can be a wonderful addition to any home, but whether you're an experienced pet parent or a
+                        first-time adopter, it's important to keep your canine companion's health and happiness a top priority.
+                        Below are some useful tips for all dog parents.
+                    </h5></br>
+                    <div class = "dog-care">
+                        <p>
+                            <img id="dog-image" alt="Dog" src="Images/dog-care_general-dog-care_main-image.jpeg"
+                                 alt="dog"></br>
+                        </p>
+                    </div>
+                    <div class = "care">
+                        <h3>Feeding</h3>
+                        <ul>
+                            <li>Puppies eight to 12 weeks old need four meals a day.</li>
+                            <li>Feed puppies three to six moths old three meals a day.</li>
+                            <li>Feed puppies six months to one year two meals a day.</li>
+                            <li>When dogs reach their first birthday, a meal a day should be enough.</li>
+                            <li>For some dogs like larger canines or those prone to bloat, two samll meals are better than one
+                                big
+                                meal.</li>
+                        </ul>
+                    </div>
+                    <div class = "care">
+                        <h3>Exercise</h3>
+                        <p>Dogs need exercise to burn calories, stimulate their minds, and stay healthy.
+                            Individual exercise needs vary based on breed or breed mix, sex, age and level of health.
+                            Exercise also tends to help dogs avoid boredom, which can lead to destructive behaviors.
+                            Supervised fun and games will satisfy many of your pet's instinctual urges to dig, herd, chew,
+                            retrieve
+                            and chase.</p>
+                    </div>
+                    <div class = "care">
+                        <h3>Fleas and Ticks</h3>
+                        <p>Daily inspections of your dog for fleas and ticks during the warm seasons are important.
+                            Use a flea comb to find and remove fleas. There are several new methods of flea and tick control.
+                            Speak to your veterinarian about these and other options.</p>
+                    </div>
+                    <div class = "care">
+                        <h3>Dog Supply Checklist</h3>
+                        <ul>
+                            <li>Dog food and treats</li>
+                            <li>Food dish</li>
+                            <li>water bowl</li>
+                            <li>Toys (including safe chew toys)</li>
+                            <li>Brush & comb (including flea comb)</li>
+                            <li>Collar and ID tag</li>
+                            <li>Dog bed</li>
+                        </ul>
+                    </div>
+                    <p class = "source">
+                        Source: “General Dog Care.” ASPCA, 2015, <a href="www.aspca.org/pet-care/dog-care/general-dog-care" >www.aspca.org/pet-care/dog-care/general-dog-care.</a>
+                    </p>
+
+                </div>
+            </div>
+        </body>
+
+        <footer>
+            <?php include 'footer.php'; ?>
+        </footer>
+    </html>
