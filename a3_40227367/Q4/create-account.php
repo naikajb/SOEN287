34f7c19fb2a6fd85da2d503f6e3aa870336bafb4
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-04-15 12:25:30 -0400

    started the sign-up page

--- a3_40227367/Q4/create-account.php
+++ a3_40227367/Q4/create-account.php
@@ -22,19 +22,31 @@
                 <p> Please fill in this form to create an account</p></br>
                 <hr>
 
-                <form action = "" method = "post" class = "createAccount">
+                <form action = "accounts.txt" method = "post" class = "createAccount">
                     <div class = "signUpInputs">
                         <label style = "font-size: 15px;font-weight: bold;">USERNAME</label>
                         <input type = "text" name = "username" placeholder="type in your username" required>
+                        <p class = "requirements" >
+                            <li style = "opacity: 70%;font-size: 10px;">Username must have only digits and letters</li>
+                        </p></br>
 
                         <label style = "font-size: 15px;font-weight: bold;"> PASSWORD </label>
                         <input type = "password" name = "password" placeholder="type in your password" required>
                     </div>
 
+                    <p class = requirements >
+                        <ul style = "opacity: 70%;font-size: 10px;">
+                            <li>At least 4 characters</li>
+                            <li>Must only contain digits and letters</li>
+                        </ul>
+                    </p>
+
                     <div class = "submission">
                         <button type="button" id="cancelbtn" class = "button" >Cancel</button>
                         <button type="submit" id="signupbtn" class = "button">Sign Up</button>
                     </div>
+
+
                 </form>
             </div>
         </div>
@@ -45,3 +57,6 @@
     </body>
 </html>
 
+<?php
+    require 'signup.php';
+?>
