ce8fc542c71bd110a5f3735273a82071190dbdae
Naika JB <107156607+naikajb@users.noreply.github.com>
2023-04-16 11:18:35 -0400

    contact page

---
+++ a3_40227367/Q4/Contact.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <Title> A Cat and Dog for All</Title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
+    <Title> A Cat and Dog for All</Title>
+    <link rel = "stylesheet" href = "CSS/style.css">
+    <script type = "text/javascript" src="JS/adopt.js"></script>
+</head>
+
+<body>
+<header>
+   <?php include 'header.php';?>
+</header>
+
+<div class = "sidebar">
+    <?php include 'sidebar.php';?>
+</div>
+
+<div class ="main">
+    <div class ="content">
+        <p><h2>CONTACT US WITH THE INFO BELOW</h2></p></br>
+
+        <div class = "info">
+            <p>Name: Naika Jean-Baptiste</p>
+            <p>Student ID: 40227367</p>
+            <p>Email: jbaptiste.naika@gmail.com</p>
+        </div>
+
+    </div>
+</div>
+
+<footer>
+    <?php include 'footer.php';?>
+</footer>
+</body>
+</html>
